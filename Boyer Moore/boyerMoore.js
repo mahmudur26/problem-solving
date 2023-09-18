890e0ce282c93ad3abad3914c6ce842b76dbf875
aurko_lapi <mahmudur.rashid26@gmail.com>
2023-09-19 00:27:48 +0600

    boyer moore bad character approach

---
+++ Boyer Moore/boyerMoore.js
@@ -0,0 +1,46 @@
+// BAD CHARACTER
+
+class BoyerMoore {
+    constructor(pattern) {
+      this.pattern = pattern;
+      this.patternLength = pattern.length;
+      this.badCharacter = this.calculateBadCharacter();
+    }
+  
+    calculateBadCharacter() {
+      const badCharacter = new Array(256).fill(-1);
+      for (let i = 0; i < this.patternLength; i++) {
+        badCharacter[this.pattern.charCodeAt(i)] = i;
+      }
+      return badCharacter;
+    }
+  
+    searchPattern(text) {
+      const patternLength = this.patternLength;
+      const textLength = text.length;
+      let shift = 0;
+      
+      while (shift <= textLength - patternLength) {
+        let j = patternLength - 1;
+        while (j >= 0 && this.pattern[j] === text[shift + j]) {
+          j--;
+        }
+        
+        if (j < 0) {
+          console.log(`Pattern found at position: ${shift}`);
+          shift += (shift + patternLength < textLength)
+            ? patternLength - this.badCharacter[text.charCodeAt(shift + patternLength)]
+            : 1;
+        } else {
+          shift += Math.max(1, j - this.badCharacter[text.charCodeAt(shift + j)]);
+        }
+      }
+    }
+  }
+  
+  // Example usage:
+  const pattern = "example";
+  const text = "This is an example text. Here's an example sentence.";
+  const bm = new BoyerMoore(pattern);
+  bm.searchPattern(text);
+  
