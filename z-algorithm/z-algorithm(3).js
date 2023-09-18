803f8c77041ddfbcfdde2350348bcce53cd54cd1
aurko_lapi <mahmudur.rashid26@gmail.com>
2023-09-18 23:50:45 +0600

    new code of z algorithm

---
+++ z-algorithm/z-algorithm(3).js
@@ -0,0 +1,42 @@
+function search(text, pattern) {
+    let concat = pattern + "$" + text;
+    let l = concat.length;
+    let Z = new Array(l);
+    getZarr(concat, Z);
+
+    for (let i = 0; i < l; ++i) {
+        if (Z[i] === pattern.length) {
+            console.log("Pattern found at index " + (i - pattern.length - 1));
+        }
+    }
+}
+
+function getZarr(str, Z) {
+    let n = str.length;
+    let L = 0, R = 0;
+    for (let i = 1; i < n; ++i) {
+        if (i > R) {
+            L = R = i;
+            while (R < n && str[R - L] == str[R])
+                R++;
+            Z[i] = R - L;
+            R--;
+        } else {
+            let k = i - L;
+            if (Z[k] < R - i + 1)
+                Z[i] = Z[k];
+            else {
+                L = i;
+                while (R < n && str[R - L] == str[R])
+                    R++;
+                Z[i] = R - L;
+                R--;
+            }
+        }
+    }
+}
+
+let text = "AACMAACXY";
+let pattern = "AAC";
+
+search(text, pattern);
